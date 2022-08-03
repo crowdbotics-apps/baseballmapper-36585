e0f18f9d6a703f277635c799f1f788e968858124
Crowdbotics <team@crowdbotics.com>
2022-08-03 18:32:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "baseballmapper_36585",
+  url: "https://baseballmapper_36585.botics.co",
+  api: "https://baseballmapper_36585.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
